417d1f0f8d898a147b5c2631e37103d9d9f5bb48
TahirAnny <tahiraanny@gmail.com>
2021-06-16 23:53:38 +0600

    refactor

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,5 +1,5 @@
-import axios, { AxiosResponse } from "axios";
-import { Activity } from "../models/activity";
+import axios, { AxiosResponse } from 'axios';
+import { Activity } from '../models/activity';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -25,13 +25,13 @@ const requests = {
     get: <T> (url: string) => axios.get<T>(url).then(responseBody),
     post: <T> (url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
     put: <T> (url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
-    delete: <T> (url: string) => axios.delete<T>(url).then(responseBody),
+    del: <T> (url: string) => axios.delete<T>(url).then(responseBody),
 }
 
 const Activities = {
     list: () => requests.get<Activity[]>('/activities'),
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
-    create: (activity: Activity) => requests.post<void>('/activities', activity),
+    create: (activity: Activity) => axios.post<void>('/activities', activity),
     update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
     delete: (id: string) => axios.delete<void>(`/activities/${id}`)
 }
@@ -40,4 +40,4 @@ const agent = {
     Activities
 }
 
-export default agent;
+export default agent; 
