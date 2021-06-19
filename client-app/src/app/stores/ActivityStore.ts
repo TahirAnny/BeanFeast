e80d783ca8edca87ef69796d5c0f788631687a6e
TahirAnny <tahiraanny@gmail.com>
2021-06-19 22:52:19 +0600

    refactoring code with MobX

--- client-app/src/app/stores/ActivityStore.ts
+++ client-app/src/app/stores/ActivityStore.ts
@@ -1,13 +1,37 @@
 import { makeAutoObservable } from "mobx";
+import agent from "../api/agent";
+import { Activity } from "../models/activity";
 
 export default class ActivityStore{
-    title = `Hello!!!`;
+    activities: Activity[] = [];
+    selectedActivity: Activity | null = null;
+    editMode= false;
+    loading= false;
+    loadingInitial= false;
 
     constructor(){
         makeAutoObservable(this)
     }
 
-    setTitle = () => {
-        this.title = this.title + `Welcome To Group!`;
+    loadActivities = async () => {
+        this.setLoadingInitial(true);
+        try{
+            const activities = await agent.Activities.list();
+           
+            activities.forEach(activity => {
+                activity.date = activity.date.split('T')[0];
+                this.activities.push(activity);
+            })
+            this.loadingInitial = false;
+            
+            this.setLoadingInitial(true);
+        } catch(error){
+            console.log(error);
+            this.setLoadingInitial(false);
+        }
+    }
+
+    setLoadingInitial = (state: boolean) => {
+        this.loadingInitial = state;
     }
 }
