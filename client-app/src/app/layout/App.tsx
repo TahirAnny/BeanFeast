c2bd489fabc521d99e14055823ba64d70c47c08e
TahirAnny <tahiraanny@gmail.com>
2021-07-01 20:41:57 +0600

    View activity details in a different page using route

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
@@ -6,6 +6,7 @@ import { observer } from 'mobx-react-lite';
 import { Route } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 function App() {
 
@@ -14,7 +15,8 @@ function App() {
       <NavBar />
       <Container style={{marginTop: '7em'}}>
           <Route exact path='/' component={HomePage}/>
-          <Route path='/activities' component={ActivityDashboard}/>
+          <Route exact path='/activities' component={ActivityDashboard}/>
+          <Route path='/activities/:id' component={ActivityDetails}/>
           <Route path='/createActivity' component={ActivityForm}/>
       </Container>
     </>
